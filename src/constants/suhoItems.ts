ebfb9f68481b735fa61fef02059ad34cdccba834
leey00nsu <dbstndla1212@naver.com>
2023-07-02 23:56:13 +0900

    feat. 꾸미기 아이템 추가 바지1,바지2,체크셔츠,정장,신발1,더벅머리,가방1,가방2,도트선글라스,별선글라스,동그란안경,다크써클,눈썹1,눈썹2 홍조1,홍조2,소주,커피,노트북

--- src/constants/suhoItems.ts
+++ src/constants/suhoItems.ts
@@ -5,13 +5,108 @@ export const suhoItems = [
     itemUrl: "/suho/suho_baseball_jumper.png",
   },
   {
+    part: "몸",
+    item: "바지1",
+    itemUrl: "/suho/suho_pants1.png",
+  },
+  {
+    part: "몸",
+    item: "바지2",
+    itemUrl: "/suho/suho_pants2.png",
+  },
+  {
+    part: "몸",
+    item: "체크셔츠",
+    itemUrl: "/suho/suho_check_shirts.png",
+  },
+  {
+    part: "몸",
+    item: "정장",
+    itemUrl: "/suho/suho_suit.png",
+  },
+  {
     part: "발",
     item: "샌들",
     itemUrl: "/suho/suho_sandle.png",
   },
   {
+    part: "발",
+    item: "신발1",
+    itemUrl: "/suho/suho_shoes1.png",
+  },
+  {
     part: "머리",
     item: "야구모자",
     itemUrl: "/suho/suho_yankees_hat.png",
+  },
+  {
+    part: "머리",
+    item: "더벅머리",
+    itemUrl: "/suho/suho_messy_hair.png",
+  },
+  {
+    part: "몸",
+    item: "가방1",
+    itemUrl: "/suho/suho_backpack1.png",
+  },
+  {
+    part: "몸",
+    item: "가방2",
+    itemUrl: "/suho/suho_backpack2.png",
+  },
+  {
+    part: "얼굴",
+    item: "도트선글라스",
+    itemUrl: "/suho/suho_pixel_sunglasses.png",
+  },
+  {
+    part: "얼굴",
+    item: "별선글라스",
+    itemUrl: "/suho/suho_star_sunglasses.png",
+  },
+  {
+    part: "얼굴",
+    item: "동그란안경",
+    itemUrl: "/suho/suho_circle_glasses.png",
+  },
+  {
+    part: "얼굴",
+    item: "다크써클",
+    itemUrl: "/suho/suho_darkcircle.png",
+  },
+  {
+    part: "얼굴",
+    item: "눈썹1",
+    itemUrl: "/suho/suho_eyebrow1.png",
+  },
+  {
+    part: "얼굴",
+    item: "눈썹2",
+    itemUrl: "/suho/suho_eyebrow2.png",
+  },
+  {
+    part: "얼굴",
+    item: "홍조1",
+    itemUrl: "/suho/suho_blush1.png",
+  },
+  {
+    part: "얼굴",
+    item: "홍조2",
+    itemUrl: "/suho/suho_blush2.png",
+  },
+  {
+    part: "손",
+    item: "소주",
+    itemUrl: "/suho/suho_soju.png",
+  },
+  {
+    part: "손",
+    item: "커피",
+    itemUrl: "/suho/suho_coffee.png",
+  },
+  {
+    part: "손",
+    item: "노트북",
+    itemUrl: "/suho/suho_laptop.png",
   },
 ];
