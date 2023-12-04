ae05935a0a0173222968b9631fe44194e49507a8
leey00nsu <dbstndla1212@naver.com>
2023-12-04 17:48:08 +0900

    refactor. 펜 색상 CSS 변수 변경 로직 useEffect로 변경

--- src/components/menu/Draw.tsx
+++ src/components/menu/Draw.tsx
@@ -1,11 +1,17 @@
 import { ChromePicker, ColorResult } from 'react-color';
 import { useRecoilState } from 'recoil';
 import { penState } from '../../store/store';
+import { useEffect } from 'react';
 
 const MAX_PEN_SIZE = 200;
 
 const Draw = () => {
   const [pen, setPen] = useRecoilState(penState);
+
+  // 펜 색상 변경시 css 변수 변경
+  useEffect(() => {
+    document.documentElement.style.setProperty('--range-shdw', pen.hsl);
+  }, [pen]);
 
   const changePenSizeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (Number(e.target.value) > MAX_PEN_SIZE) return;
@@ -26,14 +32,7 @@ const Draw = () => {
   };
 
   return (
-    <section
-      style={
-        {
-          '--range-shdw': pen.hsl,
-        } as React.CSSProperties
-      }
-      className="flex w-full grow flex-col items-center justify-center border-t  border-base-300 bg-white sm:flex-row"
-    >
+    <section className="flex w-full grow flex-col items-center justify-center border-t  border-base-300 bg-white sm:flex-row">
       <div className="flex w-full flex-col items-center gap-2 p-2 sm:w-1/2">
         <p className="text-lg font-medium ">펜 굵기</p>
         <input
