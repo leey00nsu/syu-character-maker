81f4eb87deb53ca3f465a84228efea9254143a52
leey00nsu <dbstndla1212@naver.com>
2023-12-31 17:57:06 +0900

    refactor. User 페이지 분리

---
+++ src/features/user/User.tsx
@@ -0,0 +1,38 @@
+import { useAuthStore } from '@/store/authStore';
+
+import useLogout from '@/hooks/auth/useLogout';
+
+import { Avatar } from '@/ui/avatars';
+import { ActiveButton } from '@/ui/buttons';
+import { Paragraph } from '@/ui/texts';
+
+const User = () => {
+  const user = useAuthStore(state => state.user);
+
+  const { logoutMutation } = useLogout();
+
+  const logoutHandler = async () => {
+    await logoutMutation();
+  };
+
+  return (
+    <div className="flex w-full flex-col items-center gap-4 p-4 py-10">
+      <div className="flex h-20 justify-center">
+        <Avatar photo={user.photo} />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <Paragraph size="md" weight="normal">
+          {user.email}
+        </Paragraph>
+        <Paragraph size="md" weight="normal">
+          {user.name}
+        </Paragraph>
+        <ActiveButton className="btn w-full" clickHandler={logoutHandler}>
+          로그아웃
+        </ActiveButton>
+      </div>
+    </div>
+  );
+};
+
+export default User;
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,22 +1,9 @@
-import { useAuthStore } from '@/store/authStore';
-
-import useLogout from '@/hooks/auth/useLogout';
-
 import { Header } from '@/features/header';
+import { User } from '@/features/user';
 
-import { Avatar } from '@/ui/avatars';
 import { PageContainer } from '@/ui/containers';
-import { Paragraph } from '@/ui/texts';
 
 const UserPage = () => {
-  const user = useAuthStore(state => state.user);
-
-  const { logoutMutation } = useLogout();
-
-  const logoutHandler = async () => {
-    await logoutMutation();
-  };
-
   return (
     <PageContainer>
       <PageContainer.Header>
@@ -24,22 +11,7 @@ const UserPage = () => {
       </PageContainer.Header>
 
       <PageContainer.Content>
-        <section className="flex w-full flex-col items-center gap-4 p-4 py-10">
-          <div className="flex h-20 justify-center">
-            <Avatar photo={user.photo} />
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <Paragraph size="md" weight="normal">
-              {user.email}
-            </Paragraph>
-            <Paragraph size="md" weight="normal">
-              {user.name}
-            </Paragraph>
-            <button className="btn" onClick={logoutHandler}>
-              로그아웃
-            </button>
-          </div>
-        </section>
+        <User />
       </PageContainer.Content>
     </PageContainer>
   );
