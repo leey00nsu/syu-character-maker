5ca07f0ca42ffa9f8c46734f5860b49ff5a43764
leey00nsu <dbstndla1212@naver.com>
2023-12-31 20:17:52 +0900

    fix. 대소문자 구분 오류 수정

--- src/features/canvas/components/CanvasObjects/objects/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as BackgroundObject } from './BackgroundObject';
-export { default as CharacterObject } from './CharacterObject';
-export { default as ImageObject } from './ImageObject';
-export { default as LineObject } from './LineObject';
-export { default as DecorationObject } from './decorationObject';
