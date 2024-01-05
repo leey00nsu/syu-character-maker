8c0e0404a2a403465f8f7f76a5ecec533fa85319
leey00nsu <dbstndla1212@naver.com>
2024-01-05 23:55:25 +0900

    fix. withCredentails true 설정 same-site여도 명시하지 않으면 쿠키가 넘어가지 않는다

--- src/apis/axios.config.ts
+++ src/apis/axios.config.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-// 개발 환경에서 크로스 도메인에서 쿠키를 전송할 수 있도록 설정
-
 const axiosInstance = axios.create({
-  withCredentials: import.meta.env.DEV,
+  withCredentials: true,
 });
 
 export default axiosInstance;
